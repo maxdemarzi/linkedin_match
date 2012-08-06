670e8a4f542cf52ef9cacd397dc2c0e3b0bd5425
Max De Marzi <maxdemarzi@hotmail.com>
2012-08-06 15:25:40 -0500

    add full text index

--- lib/cbm/util.rb
+++ lib/cbm/util.rb
@@ -5,6 +5,8 @@ module CBM
     # Import values, 50 at a time.
     def self.import(model, properties, values)
       puts "Importing #{model}"
+      @neo_server.create_node_index("#{model}_index", "fulltext", "lucene")
+
       values.each_slice(50) do |slice|
         commands = []
         slice.each do |value|
@@ -15,6 +17,16 @@ module CBM
           commands << [:create_unique_node, "#{model}_index", "ref_id", node_properties[:id], node_properties]
 
         end
+
+        slice.each_with_index do |value, index|
+          node_properties = Hash.new
+          properties.each_with_index do |p,index|
+            node_properties[p]= value[index]
+          end
+
+          commands << [:add_node_to_index, "#{model}_index", "name", node_properties[:name], "{#{index}}"]
+        end
+
         @neo_server.batch *commands
       end
 
